93538031826075c04a64633273aff3c0e91d7562
Brayden <braydenthompson@u.boisestate.edu>
2022-03-10 00:52:07 -0700

    Updated HashTable

--- src/HashTable.java
+++ src/HashTable.java
@@ -1,9 +1,16 @@
 public class HashTable<T>
 {
-    private HashObject[] table;
+    private HashObject[] table;     // The table in which to store HashObjects
+    private boolean doubleHash;     // True if using double hashing, false if using linear hashing.
 
-    public HashTable (int size, boolean)
+    /**
+     * Default constructor for a new HashTable object
+     * @param size the capacity of the table
+     * @param doubleHash the hashing mode - true for double hashing, false for linear hashing
+     */
+    public HashTable (int size, boolean doubleHash)
     {
         table = new HashObject[size];
+        this.doubleHash = doubleHash;
     }
 }
